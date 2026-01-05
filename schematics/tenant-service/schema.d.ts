65791742baf963d8448f754931d736cb5c34d996
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2026-01-05 13:14:09 -0500

    chore: Update schematics schema types

--- schematics/tenant-service/schema.d.ts
+++ schematics/tenant-service/schema.d.ts
@@ -1,18 +1,18 @@
 export interface TenantServiceOptions {
-  /**
-   * The name of the service (e.g., 'users', 'projects')
-   */
-  name: string;
-  /**
-   * The path to create the service
-   */
-  path?: string;
-  /**
-   * Generate file without creating a folder
-   */
-  flat?: boolean;
-  /**
-   * Skip generating test file
-   */
-  skipTests?: boolean;
+    /**
+     * The name of the service (e.g., 'users', 'projects')
+     */
+    name: string;
+    /**
+     * The path to create the service
+     */
+    path?: string;
+    /**
+     * Generate file without creating a folder
+     */
+    flat?: boolean;
+    /**
+     * Skip generating test file
+     */
+    skipTests?: boolean;
 }
